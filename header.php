3e76c1535b8146c9c17a953eef0529597fcc3a7e
SRIDHALA J S <116045649+SRIDHALA@users.noreply.github.com>
2023-02-14 06:26:15 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php
+
+    $server = 'localhost';
+    $username = 'SRIDHALA';
+    $password = 'Sri@2003';
+    $dbname = 'univ1';
+
+    $conn = mysqli_connect($server, $username, $password, $dbname);
+?>
